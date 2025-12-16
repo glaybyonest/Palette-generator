c03bb82cd9d3213bada7c04e93f55ce4444f3a68
glaybyonest <daniilsetskov@yandex.ru>
2025-12-16 17:22:53 +0300

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/palette-generator/'
+  base: '/Palette-generator/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
